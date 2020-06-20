ee6fbbf9dd6437ef4cc03a6037db3389e2d406d4
Anton Krylov <Ant.Krylov@x5.ru>
2020-06-20 17:46:02 +0300

    add styles

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './App.css';
 
-import { GET_ALL_RECIPES } from '../../queries';
-import { Query } from 'react-apollo';
+import {GET_ALL_RECIPES} from '../../queries';
+import {Query} from 'react-apollo';
 
-const App = () => (
-    <div className="App">
-        <h1>Home</h1>
-        <Query query={GET_ALL_RECIPES}>
-            {({ data, loading, error }) => {
-                if (loading) return <div>Loading</div>
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Home</h1>
+            <Query query={GET_ALL_RECIPES}>
+                {({data, loading, error}) => {
+                    if (loading) return <div>Loading</div>
 
-                if (error) return <div>Error</div>
+                    if (error) return <div>Error</div>
 
-                console.log(data);
-
-                return (<p>{data}</p>)
-            }}
-        </Query>
-    </div>
-);
+                    return (<p>Recipe</p>)
+                }}
+            </Query>
+        </div>
+    )
+};
 
 export default App;
