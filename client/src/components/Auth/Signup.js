68e8903a1b3fa6f5aa286eb05eb57af48fb596ee
Anton Krylov <Ant.Krylov@x5.ru>
2020-06-20 18:43:27 +0300

    Add state to signup

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
 
 class Signup extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+    }
+
+    handleChange = e => {
+        const { target: { value, name } } = e;
+        this.setState({
+            [name]: value
+        });
+    }
+
     render() {
+        const { email, username, password, passwordConfirm } = this.state;
+
         return (
             <div className={"App"}>
                 <h2 className="App">SignUp</h2>
                 <form action="" className="form">
-                    <input type="text" name="username" placeholder="Username"/>
-                    <input type="email" name="email" placeholder="Email Address"/>
-                    <input type="password" name="password" placeholder="Password"/>
-                    <input type="password" name="passwordConfirm" placeholder="Password confirmation"/>
+                    <input type="text" name="username" value={email} placeholder="Username" onChange={this.handleChange}/>
+                    <input type="email" name="email" value={username} placeholder="Email Address" onChange={this.handleChange}/>
+                    <input type="password" name="password" value={password} placeholder="Password" onChange={this.handleChange}/>
+                    <input type="password" name="passwordConfirm" value={passwordConfirm} placeholder="Password confirmation" onChange={this.handleChange}/>
                     <button type="submit" className="button-primary">
                         Submit
                     </button>
