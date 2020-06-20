680f7ccf466cdcf0dfd0ec241ca6baa868bf5aa0
Anton Krylov <Ant.Krylov@x5.ru>
2020-06-20 16:12:50 +0300

    create addRecipe mutation

--- resolvers.js
+++ resolvers.js
@@ -3,5 +3,16 @@ exports.resolvers = {
         getAllRecipes: () => {
 
         }
+    },
+    Mutation: {
+        addRecipe: async (root, { name, description, category, instructions, username }, { Recipe }) => {
+            return await new Recipe({
+                name,
+                description,
+                category,
+                instructions,
+                username
+            }).save();
+        }
     }
 };
--- schema.js
+++ schema.js
@@ -22,4 +22,14 @@ type Query {
     getAllRecipes: [Recipe]
 }
 
+type Mutation {
+    addRecipe(name: String!,
+    category: String!,
+    description: String!,
+    instructions: String!,
+    createdDate: String,
+    likes: Int,
+    username: String): Recipe
+}
+
 `;
